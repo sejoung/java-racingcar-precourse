0a0bb78dfd87bbbaecdbd8a5d23361499fc287ad
sejoung kim <sejoung@gmail.com>
2021-05-02 00:42:46 +0900

    feat: 시도횟수 메시지 확인

--- src/main/java/io/github/sejoung/RacingCarGame.java
+++ src/main/java/io/github/sejoung/RacingCarGame.java
@@ -1,10 +1,17 @@
 package io.github.sejoung;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCarGame {
 
 	private final StringBuffer buffer = new StringBuffer();
+	private final List<RacingCar> racingCars = new ArrayList<>();
+	private static final String CAR_NAME_SEPARATOR = ",";
+	private final CarForwardSelector selector;
 
 	public RacingCarGame(CarForwardSelector selector) {
+		this.selector = selector;
 		this.buffer.append("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 	}
 
@@ -19,8 +26,26 @@ public class RacingCarGame {
 	}
 
 	public void processInput(String input) {
-		if(input.split(",").length == 1){
-			buffer.append("최소 2대 이상의 자동차가 필요합니다. 이름은 쉼표(,) 기준으로 구분");
+		int carSize = racingCars.size();
+		if (carSize == 0 && carNameValidation(input)) {
+			createRacingCar(input);
 		}
 	}
+
+	private boolean carNameValidation(String input) {
+		if (input.split(CAR_NAME_SEPARATOR).length == 1) {
+			buffer.append("최소 2대 이상의 자동차가 필요합니다. 이름은 쉼표(,) 기준으로 구분");
+			return false;
+		}
+		buffer.append("시도할 회수는 몇회인가요?");
+		return true;
+	}
+
+	private void createRacingCar(String input) {
+		String[] carNames = input.split(CAR_NAME_SEPARATOR);
+		for (String carName : carNames) {
+			racingCars.add(new RacingCar(selector, carName));
+		}
+	}
+
 }
