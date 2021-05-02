cb3777d9925b0e3970033ac962ec46656f32902e
sejoung kim <sejoung@gmail.com>
2021-05-02 09:29:01 +0900

    refactor :  출력 메소드 정리

--- src/main/java/io/github/sejoung/RacingCarGame.java
+++ src/main/java/io/github/sejoung/RacingCarGame.java
@@ -10,10 +10,11 @@ public class RacingCarGame {
 	private static final String CAR_NAME_SEPARATOR = ",";
 	private final PositiveIntegerMinZeroMaxNineGenerator selector;
 	private int racingCycle;
+	private final String LINE_SEPARATOR = System.lineSeparator();
 
 	public RacingCarGame(PositiveIntegerMinZeroMaxNineGenerator selector) {
 		this.selector = selector;
-		this.buffer.append("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		this.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 	}
 
 	public boolean isCompleted() {
@@ -40,22 +41,22 @@ public class RacingCarGame {
 		try {
 			this.racingCycle = Integer.parseInt(input);
 			if (this.racingCycle == 0) {
-				buffer.append("시도 횟수는 최소 1이상의 값이다.");
+				print("시도 횟수는 최소 1이상의 값이다.");
 				return false;
 			}
 			return true;
 		} catch (NumberFormatException e) {
-			buffer.append("숫자만 입력할수 있습니다.");
+			print("숫자만 입력할수 있습니다.");
 			return false;
 		}
 	}
 
 	private boolean carNameValidation(String input) {
 		if (input.split(CAR_NAME_SEPARATOR).length == 1) {
-			buffer.append("최소 2대 이상의 자동차가 필요합니다. 이름은 쉼표(,) 기준으로 구분");
+			print("최소 2대 이상의 자동차가 필요합니다. 이름은 쉼표(,) 기준으로 구분");
 			return false;
 		}
-		buffer.append("시도할 회수는 몇회인가요?");
+		print("시도할 회수는 몇회인가요?");
 		return true;
 	}
 
@@ -64,6 +65,10 @@ public class RacingCarGame {
 		for (String carName : carNames) {
 			racingCars.add(new RacingCar(selector, carName));
 		}
+	}
+
+	private void print(String message) {
+		buffer.append(message);
 	}
 
 }
